a69e40e636dae8b11b41b3d9ab0efb77881d3199
YOUR NAME <YOUR@EMAIL.com>
2016-01-17 16:25:16 -0600

    Store Update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,39 @@
 class ProductsController < ApplicationController
 
-	def displayitem
-		@product = Product.all
+	def index
+		@products = Product.all
+	end
+
+	def show 
+		@product = Product.find_by(id: params[:id])
+	end
+
+	def new
+	end
+
+	def create
+		@prodcut = Product.create({name: params[:name],
+								price: params[:price],
+								image: params[:image],
+								description: params[:description]})
+	end
+
+	def edit 
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+
+		@product.update({name: params[:name],
+								price: params[:price],
+								image: params[:image],
+								description: params[:description]})
+	end
+
+	def destroy
+		@product = product.find(params[:id])
+		@product.destroy
 	end
 end
+
